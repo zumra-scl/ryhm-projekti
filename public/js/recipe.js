e7938d7ef26e27fcc63ec2d84e583e1cc218db21
Zumra <s2401127@edu.bc.fi>
2026-04-30 13:16:32 +0300

    details

--- public/js/recipe.js
+++ public/js/recipe.js
@@ -1,5 +1,4 @@
-const API_URL = "http://localhost:3000/api/recipes/";
-
+const API_URL = "http://localhost:3000/recipes/";
 const params = new URLSearchParams(window.location.search);
 const id = params.get("id");
 
